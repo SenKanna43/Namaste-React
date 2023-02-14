33109dffd7ad9ca9d8c10a6ea1fae3868cc887e2
senthamaraikannan43 <senthamaraikannan@pganalytics.in>
2023-02-14 17:37:35 +0530

    repo creted

---
+++ App.js
@@ -0,0 +1,9 @@
+
+const result = React.createElement(
+    'h1',
+    { id: 'title' },
+    "Hello World")
+const root = ReactDOM.createRoot(
+    document.getElementById('root')
+)
+root.render(result)
